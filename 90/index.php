744c89bb4ad4b95f7d87cc247171b3c4c3b88700
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-24 20:49:35 +0330

    100-construct-destruct

--- 90/index.php
+++ 90/index.php
@@ -1,50 +1,72 @@
 <?php
 
+// class fruit
+// {
+
+//     public $name;
+//     public $color;
+
+//     function set_name($fruitName, $fruitColor)
+//     {
+//         $this->name = $fruitName;
+//         $this->color = $fruitColor;
+//     }
+
+//     function get_name()
+//     {
+//         return $this->name;
+//     }
+//     function get_color()
+//     {
+//         return $this->color;
+//     }
+
+//     function get_info()
+//     {
+//         return $this->name . " - " . $this->color;
+//     }
+// }
+
+// $apple = new fruit();
+
+// $apple->set_name("apple", "red");
+
+// var_dump($apple);
+
+// echo "<br>";
+
+// $banana = new fruit();
+
+// $banana->set_name("banana", "yellow");
+
+// echo $banana->get_color();
+
+// echo "<br>";
+
+// echo $banana->get_name();
+
+// echo "<br>";
+
+// echo $banana->get_info();
+
+
 class fruit
 {
 
     public $name;
     public $color;
 
-    function set_name($fruitName, $fruitColor)
+    function __construct($friutName, $friutColor)
     {
-        $this->name = $fruitName;
-        $this->color = $fruitColor;
+        $this->name = $friutName;
+        $this->color = $friutColor;
     }
-
-    function get_name()
+    function __destruct()
     {
-        return $this->name;
-    }
-    function get_color()
-    {
-        return $this->color;
-    }
-
-    function get_info()
-    {
-        return $this->name . " - " . $this->color;
+        echo "the fruit is {$this->name} and the color is {$this->color}";
     }
 }
 
-$apple = new fruit();
-
-$apple->set_name("apple", "red");
-
-var_dump($apple);
-
-echo "<br>";
-
-$banana = new fruit();
-
-$banana->set_name("banana", "yellow");
-
-echo $banana->get_color();
-
-echo "<br>";
-
-echo $banana->get_name();
-
-echo "<br>";
-
-echo $banana->get_info();
+$apple = new fruit("apple", "red");
+ 
+// var_dump($apple);// this use for construct no usefull for destruct
