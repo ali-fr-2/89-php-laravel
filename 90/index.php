77160bc48f7b9cfeff86cf23f37e77a866d0a08b
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-25 20:53:54 +0330

    101-extends

--- 90/index.php
+++ 90/index.php
@@ -1,72 +1,15 @@
 <?php
 
-// class fruit
-// {
-
-//     public $name;
-//     public $color;
-
-//     function set_name($fruitName, $fruitColor)
-//     {
-//         $this->name = $fruitName;
-//         $this->color = $fruitColor;
-//     }
-
-//     function get_name()
-//     {
-//         return $this->name;
-//     }
-//     function get_color()
-//     {
-//         return $this->color;
-//     }
-
-//     function get_info()
-//     {
-//         return $this->name . " - " . $this->color;
-//     }
-// }
-
-// $apple = new fruit();
-
-// $apple->set_name("apple", "red");
-
-// var_dump($apple);
-
-// echo "<br>";
-
-// $banana = new fruit();
-
-// $banana->set_name("banana", "yellow");
-
-// echo $banana->get_color();
-
-// echo "<br>";
-
-// echo $banana->get_name();
-
-// echo "<br>";
-
-// echo $banana->get_info();
-
-
-class fruit
-{
-
-    public $name;
-    public $color;
-
-    function __construct($friutName, $friutColor)
-    {
-        $this->name = $friutName;
-        $this->color = $friutColor;
-    }
-    function __destruct()
-    {
-        echo "the fruit is {$this->name} and the color is {$this->color}";
-    }
+class parents{
+    public $txt="my eye is blue";
 }
 
-$apple = new fruit("apple", "red");
- 
-// var_dump($apple);// this use for construct no usefull for destruct
+class child extends parents{
+    public function eyecolor(){
+        return $this->txt;
+    } 
+}
+
+$ali=new child();
+
+echo $ali->eyecolor();
