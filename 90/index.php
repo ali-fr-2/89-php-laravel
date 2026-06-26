1fccf2317602367d9d539d2d749919d546424477
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-26 17:01:00 +0330

    106-trait

--- 90/index.php
+++ 90/index.php
@@ -1,20 +1,55 @@
 <?php
 
-interface animal{
-    public function makesound();
+trait msg1{
+    public function message1(){
+        echo "hello world";
+    }
+}
+trait msg2{
+    public function message2(){
+        echo "my name is ali";
+    }
+}
+class msg3{
+    use msg1,msg2;
 }
 
-class cat implements animal{
-    #[Override]
-    public function makesound()
-    {
-        echo "meow";
+// $msg=new msg3();
+// $msg->message2();
+
+
+trait hello{
+    public function sayhello(){
+        echo "hello";
+    }
+    public function sayhi(){
+        echo "hi";
+    }
+}
+trait bye{
+    public function saygoodbye(){
+        echo "goodbye";
+    }
+    public function saybye(){
+        echo "bye";
     }
 }
 
-$animal=new cat();
+class base{
+    use hello,bye,msg1,msg2;
+}
 
-$animal->makesound();
+$test=new base();
+
+
+// $test->saygoodbye();
+// $test->saybye();
+// $test->sayhello();
+// $test->sayhi();
+
+$test->message1();
+$test->message2();
+
 
 
 ?>
