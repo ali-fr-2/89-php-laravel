f0453bc953042a45bd52c835e1f8b1c2960b8526
ali-fr-2 <alifarjamy2@gmail.com>
2026-06-26 15:30:39 +0330

    103-static

--- 90/index.php
+++ 90/index.php
@@ -1,66 +1,24 @@
 <?php
 
+// class fruits{
+//     public $name="mango";
+// }
+// $mango=new fruits();
+// var_dump($mango);
+
+
 class fruits{
-    public $name;//we can use public any where we want 
-    protected $color;//we cant use protected anywhere except in this class and with the extends classes (inheritance)
-    private $weight;//we cant use private anywhere excepts in this class 
+    public static $name="apple";
 }
 
-// class parents{
-//     public $txt="my eye is blue public";
-// }
 
-// class child extends parents{
-//     public function eyecolor(){
-//         return $this->txt;
-//     } 
-// }
-
-// $ali=new child();
-
-// echo $ali->eyecolor();
-//------------------------------------we use public and its true
-// class parents{
-//     protected $txt="my eye is blue protected";
-// }
-
-// class child extends parents{
-//     public function eyecolor(){
-//         return $this->txt;
-//     } 
-// }
-
-// $ali=new child();
-
-// echo $ali->eyecolor();
-//------------------------------------we use protected and its true
-// class parents{
-//     protected $txt="my eye is blue protected";
-// }
-
-// class child extends parents{
-//     private function eyecolor(){
-//         return $this->txt;
-//     } 
-// }
-
-// $ali=new child();
-
-// echo $ali->eyecolor();
-//------------------------------we use private and its not true
+echo fruits::$name;
 
 
-// class fruits{
-//     // public $name;//we can use public any where we want 
-//     // protected $color;//we cant use protected anywhere except in this class and with the extends classes (inheritance)
-//     private $weight="300gr";//we cant use private anywhere excepts in this class 
+class showName{
+    public static function ali(){
+        echo "my name is ali";
+    }
+}
 
-//     function getName(){
-//         return $this->weight;
-//     }
-
-// }
-
-// $apple=new fruits();
-
-// echo $apple->getName();
+showName::ali();
